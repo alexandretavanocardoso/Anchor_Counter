56a5f848f1d6e248034f3ab44a56656fec208671
alexandretavanocardoso <AlexandreTavanoDeveloper@outlook.com>
2024-09-24 02:04:10 -0300

    Criando counter

---
+++ programs/counter/src/lib.rs
@@ -0,0 +1,54 @@
+use anchor_lang::prelude::*;
+
+declare_id!("73eWZimCsEzFe3HhiryFq1G1EZWDUMj3rFaKumqnFTVi");
+
+#[program]
+pub mod anchor_counter {
+    use super::*;
+
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count = 0;
+        msg!("Counter Account Created");
+        msg!("Current Count: { }", counter.count);
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Update>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        msg!("Previous counter: {}", counter.count);
+        counter.count = counter.count.checked_add(1).unwrap();
+        msg!("Counter incremented. Current count: {}", counter.count);
+        Ok(())
+    }
+
+    pub fn decrement(ctx: Context<Update>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        msg!("Previous counter: {}", counter.count);
+        counter.count = couter.count.checked_sub(1).unwrap_or(0);
+        msg!("Counter decrement. Current count: {}", counter.count);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 8)]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+    pub user: Signer<'info>,
+}
+
+#[account]
+pub struct Counter {
+    pub count: u64,
+}
